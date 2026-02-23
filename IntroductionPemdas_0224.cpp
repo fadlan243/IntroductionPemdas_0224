15afef4f2ff7bde762989ccac40bc76751627d81
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-02-23 18:40:36 +0700

    add library

--- IntroductionPemdas_0224.cpp
+++ IntroductionPemdas_0224.cpp
@@ -1,15 +1,2 @@
 #include <iostream>
 using namespace std;
-
-//variabel global
-int sks;
-
-int main(){
-    //variabel lokal
-    int umur;
-    umur = 20;
-
-    cout << "Halo Mas Abra" << endl;
-    cout << 10 << endl;
-    cout << umur;
-} 
